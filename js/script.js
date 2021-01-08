88ab9d10eb528c128a4f4dd87be83d59ff1fbb0b
Anton Bibergal <nathgilt@gmail.com>
2021-01-08 18:26:35 +0200

    animation fix/JS

--- js/script.js
+++ js/script.js
@@ -5,8 +5,25 @@ const showMenu = (toggleId, navId) => {
     nav = document.getElementById(navId);
 
   if (toggle && nav) {
-    toggle.addEventListener("click", () => {
-      nav.classList.toggle("show");
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show');
     });
   }
 };
+
+showMenu('nav-toggle', 'nav-menu');
+
+//Remove menu mobile
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+  //Action link
+  navLink.forEach((n) => n.classList.remove('active'));
+  this.classList.add('active');
+
+  //remove menu mobile
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show');
+}
+
+navLink.forEach((n) => n.addEventListener('click,', linkAction));
