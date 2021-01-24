e5c19d59c8c2a2fb16de02eee6445b98c1220f5e
Anton Bibergal <nathgilt@gmail.com>
2021-01-24 20:37:28 +0200

    SCSS update 1.2 + html fixes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// Show menu //
-
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId);
@@ -13,15 +11,12 @@ const showMenu = (toggleId, navId) => {
 
 showMenu('nav-toggle', 'nav-menu');
 
-//Remove menu mobile
 const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
-  //Action link
   navLink.forEach((n) => n.classList.remove('active'));
   this.classList.add('active');
 
-  //remove menu mobile
   const navMenu = document.getElementById('nav-menu');
   navMenu.classList.remove('show');
 }
